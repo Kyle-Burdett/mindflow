09bc7a9f0ac690f143c0c29d5ba20aa8ddca752d
kyle-methoddata <kyle@methoddata.com>
2025-10-19 19:48:59 +0200

    Updating main.dart and pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,66 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'onboarding/welcome_page.dart';
-import 'onboarding//about_you_page.dart';
-import 'onboarding/wellness_goals_page.dart';
-import 'onboarding/planning_page.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mindflow/daily_check_in.dart';
+import 'package:mindflow/firebase_options.dart';
+import 'package:mindflow/home-nav.dart';
+import 'package:mindflow/signIN-signUP.dart';
+import 'package:mindflow/splash.dart';
+import 'package:mindflow/working_hours_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: '/get-started',
+        builder: (context, state) => const GetStartedPage(),
+      ),
+      GoRoute(
+        path: '/sign-in',
+        builder: (context, state) => const SignInPage(),
+      ),
+      GoRoute(
+        path: '/sign-up',
+        builder: (context, state) => const SignUpPage(),
+      ),
+      GoRoute(
+        path: '/home',
+        builder: (context, state) => const MainHomeScreen(),
+      ),
+      GoRoute(
+        path: '/check-in',
+        builder: (context, state) => const DailyCheckIn(),
+      ),
+      GoRoute(
+        path: '/check-in-hours',
+        builder: (context, state) => const WorkingHoursPage(),
+      ),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Navigation',
+    return MaterialApp.router(
+      title: 'Mindflow',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFDB863B)),
       ),
-
-      initialRoute: '/',
-      routes: {
-        '/': (context) => WelcomeScreen(),
-        '/tellus': (context) => TellUsScreen(),
-        '/wellness': (context) => WellnessGoalsScreen(),
-        '/planning': (context) => PlanningScreen(),
-      },
+      routerConfig: _router,
     );
   }
 }
