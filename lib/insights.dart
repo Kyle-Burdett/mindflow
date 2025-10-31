8ea07511a2014856790e9d26dd1357c03eea67fa
kyle-methoddata <kyle@methoddata.com>
2025-10-31 05:05:44 +0200

    Improving insights + Fixing reminder update

--- lib/insights.dart
+++ lib/insights.dart
@@ -125,7 +125,7 @@ class _InsightsPageState extends State<InsightsPage> {
                 percent: productivityLevel,
                 description:
                     "Over the past 7 days, you have had a productivity score of %${(productivityLevel * 100).toStringAsFixed(0)}",
-                footer: productivityLevel >= 0.5 || productivityLevel == 0 ? "No actions recommended" : "Check out these resources to improve your productivity",
+                footer: productivityLevel >= 0.7 || productivityLevel == 0 ? "No actions recommended" : productivityLevel >= 0.5 ? "No actions recommended. Resources are available" : "Check out these resources to improve your productivity",
               ),
 
               const SizedBox(height: 16),
@@ -141,7 +141,7 @@ class _InsightsPageState extends State<InsightsPage> {
                   locator<CheckInViewModel>().weeklyInsights!.totalOvertimeHours > 0 
                   ? "Over the past 7 days, you’ve worked ${locator<CheckInViewModel>().weeklyInsights?.totalOvertimeHours} hours overtime"
                   : "Over the past 7 days, you have had a Work/Life Balance score of %${(workLifeBalanceLevel * 100).toStringAsFixed(0)}",
-                footer: workLifeBalanceLevel >= 0.5 || workLifeBalanceLevel == 0 ? "No actions recommended" : "Check out these tricks for managing working hours",
+                footer: workLifeBalanceLevel >= 0.7 || workLifeBalanceLevel == 0 ? "No actions recommended" : workLifeBalanceLevel >= 0.5 ? "No actions recommended. Resources are available" : "Check out these tricks for managing working hours",
                 showArrow: true,
               ),
 
@@ -157,7 +157,7 @@ class _InsightsPageState extends State<InsightsPage> {
                 description:
                     "Over the past 7 days, you have had an Isolation score of %${(isolationLevel * 100).toStringAsFixed(0)}",
                 footer:
-                   isolationLevel >= 0.5 || isolationLevel == 0 ? "No actions recommended" : "Read about how to improve that when working from home.",
+                   isolationLevel >= 0.7 || isolationLevel == 0 ? "No actions recommended" : isolationLevel >= 0.5 ? "No actions recommended. Resources are available" : "Read about how to improve that when working from home.",
                 showArrow: true,
               ),
 
@@ -173,7 +173,7 @@ class _InsightsPageState extends State<InsightsPage> {
                 description:
                     "Over the past 7 days, you have had an energy score of %${(energyLevel * 100).toStringAsFixed(0)}",
                 footer:
-                    energyLevel >= 0.5 || energyLevel == 0 ? "No actions recommended" : "If you feel you need to, check out some additional resources on how you can improve this",
+                    energyLevel >= 0.7 || energyLevel == 0 ? "No actions recommended" : energyLevel >= 0.5 ? "No actions recommended. Resources are available" : "If you feel you need to, check out some additional resources on how you can improve this",
                 showArrow: false,
               ),
 
@@ -199,7 +199,7 @@ class _InsightsPageState extends State<InsightsPage> {
   }) {
     return GestureDetector(
       onTap: () {
-        if (percent < 0.5) {
+        if (percent < 0.7) {
           context.go('/home-second');
         }
       },
@@ -208,8 +208,8 @@ class _InsightsPageState extends State<InsightsPage> {
           color: Colors.white,
           borderRadius: BorderRadius.circular(16),
           border: Border.all(
-            color: title == "Productivity" ? Colors.blueAccent : Colors.transparent,
-            width: title == "Productivity" ? 2 : 0,
+            color: percent > 0.7 ? Colors.blueAccent : Colors.transparent,
+            width: percent > 0.7 ? 2 : 0,
           ),
           boxShadow: [
             BoxShadow(
@@ -241,9 +241,9 @@ class _InsightsPageState extends State<InsightsPage> {
                       Text(title,
                           style: const TextStyle(
                               fontSize: 16, fontWeight: FontWeight.bold)),
-                      if (showArrow)
+                      if (percent < 0.7)
                         const Spacer(),
-                      if (showArrow)
+                      if (percent < 0.7)
                         const Icon(Icons.arrow_forward_ios, size: 14),
                     ],
                   ),
